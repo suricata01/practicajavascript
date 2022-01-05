70c2843186498b162989830d5d473adaeabb88d0
suricata01 <saavedrahelmut@gmail.com>
2022-01-05 16:04:53 -0600

    Se integro la funcion con el HTML

--- descuentos.js
+++ descuentos.js
@@ -5,3 +5,16 @@ function CalcularPrecioConDescuento(precio,descuento){
     
     return PrecioconDescuento; 
 }
+function Calcular_Precio_Descuento(){
+    const inputPrice = document.getElementById("InputPrice");
+    const PriceValue = inputPrice.value;
+
+    const InputDiscount= document.getElementById("InputDiscount");
+    const DiscountValue = InputDiscount.value;
+
+    const PrecioconDescuento = CalcularPrecioConDescuento(PriceValue,DiscountValue);
+    const resultPrice = document.getElementById("ResultPrice");
+
+    resultPrice.innerText = "El precio con descuento es: " + PrecioconDescuento;
+    
+}
